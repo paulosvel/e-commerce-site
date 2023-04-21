37b435dc8d6481119bc18869dacd13196d93d69c
paulosvel <paul2001vel@gmail.com>
2023-04-21 16:21:51 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Box, Button } from "@mui/material";
+const Header = () => {
+    return (
+<Box>
+    Products
+    <Button>
+        MyCart
+    </Button>
+    </Box>
+    );
+}
+export default Header;
